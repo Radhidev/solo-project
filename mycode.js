1a30e0b6bf4dc1964369beb0327f03ad8f045a88
Radhidev <radhicode@hotmail.com>
2021-12-10 10:18:01 +0100

    last version

--- mycode.js
+++ mycode.js
@@ -1,29 +1,42 @@
 /*withdrawcode*/
 var ballance =$("#lastname").val()
 $("#twenty").click(function(){
+    var audio = document.getElementById('audio20')
     var y = ballance -20
-alert( "your new ballance is "+ y)})
+    audio.play()
+    alert( "your new ballance is "+ y)
+
+})
 
 var ballance =$("#lastname").val()
 $("#fifty").click(function(){
+    var audio = document.getElementById('audio50')
     var y = ballance -50
+    audio.play()
 alert( "your new ballance is "+ y)})
 
 var ballance =$("#lastname").val()
 $("#seventy").click(function(){
+    var audio = document.getElementById('audio70')
     var y = ballance -70
+    audio.play()
 alert( "your new ballance is "+ y)})
 
 var ballance =$("#lastname").val()
 $("#onehundred").click(function(){
+    var audio = document.getElementById('audio100')
     var y = ballance -100
+    audio.play()
 alert( "your new ballance is "+ y)})
 /*depositcode*/
 $("#depositconfirm").click(function(){
     var deposit=$("#depositnumber").val()
-
     var y = ballance+deposit
-alert( "your new ballance is "+y)})
+alert("your new ballance is "+y)
+$("audio20").append()
+})
+
+
 
 
 
